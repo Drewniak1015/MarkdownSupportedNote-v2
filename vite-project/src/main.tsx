87553344d04b246c2ac2102be106dc532f6443de
Jaroslaw <Jarositek123456@gmail.com>
2025-03-09 17:40:14 +0100

    something blue

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename="/">
+    <HashRouter basename="/MarkdownSupportedNote-v2">
       <App />
     </HashRouter>
   </StrictMode>
